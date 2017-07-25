145612ae31d4319e91a95d7cef0374f17a0d8438
acolemanASC4 <npaaa5@gmail.com>
2017-07-25 14:49:34 -0400

    HTML

--- bounce/bounce.js
+++ bounce/bounce.js
@@ -1,35 +1,21 @@
 var v = Math.floor(Math.random()*255)
 var q = Math.floor(Math.random()*255)
 var p = Math.floor(Math.random()*255)
-
 function setup() {
     cnv = createCanvas(windowWidth, windowHeight)
     background(v,q,p)
 }
-
 var x = 100
 var y = 100
+var s = 100
 var xIncrement = 5
-var yIncrement = 1
-var ellipser = 50
-var edge = false;
-var windowWidths = windowWidth 
-if (ellipser > windowWidths -10 || ellipser < 10) {
-            edge = true
-        }
-
-if (edge) {
-    cnv = createCanvas(windowWidth, windowHeight)
-    background(v,q,p)
-    }
-
+var yIncrement = 5
 function draw(){
     background(v,q,p)
     fill(q,v,p)
-    ellipse(x,y)
+    ellipse(x,y,s)
     x += xIncrement;
     y += yIncrement
-
     if (x >= windowWidth - 50 || x <= 50){
         xIncrement = -xIncrement
     }
@@ -37,6 +23,3 @@ function draw(){
         yIncrement = -yIncrement
     }
 }
-
-
-
